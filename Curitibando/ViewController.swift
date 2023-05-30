c1746e851e91def542c99442ce39045963b3c5e0
lafelipe23 <felipediasapps@gmail.com>
2023-05-30 16:06:58 -0300

    Adding initial pods for google maps and configuring Map View Controller

--- Curitibando/ViewController.swift
+++ Curitibando/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UISearchResultsUpdating {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Maps"
+        view.addSubview(mapView)
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        setMapLocation()
+    }
+    
+    func setMapLocation() {
+        let initialLocation = CLLocationCoordinate2D(latitude: -25.432777498282327, longitude: -49.27585744094422)
+        let region = MKCoordinateRegion(center: initialLocation, latitudinalMeters: 14000, longitudinalMeters: 14000)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func setPinLocation(with latitude: Double, with longitude: Double) {
+        let pin = MKPointAnnotation()
+        let coordinates = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        pin.coordinate = coordinates
+        mapView.addAnnotation(pin)
+        
+        // Zoom into the selected location
+        let region = MKCoordinateRegion(center: coordinates, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        return
+    }
+    
 }
-
---
+++ Pods/Target Support Files/Pods-Curitibando/Pods-Curitibando-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Curitibando : NSObject
+@end
+@implementation PodsDummy_Pods_Curitibando
+@end
